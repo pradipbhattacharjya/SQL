7e945b4489af5bf3f103e21c956759b8883a0822
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-28 10:23:29 +0530

    the total score and total number

---
+++ Query Data (SELECT)/Group by 2.sql
@@ -0,0 +1,7 @@
+--Find the total score and total number of customers for each country.
+SELECT
+	country,
+	SUM(score) AS total_score,
+	COUNT(id) AS total_customers
+FROM customers
+GROUP BY country
